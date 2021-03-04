f3d1c197a45076dcb28bd8b0f65ceeaa3a4b7878
Kristoffer-Martinsen <kristoffermartinsen.95@gmail.com>
2021-03-04 15:06:18 +0100

    first commit

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle and pan"},
+                new Command{Id=1, HowTo="Cut Bread", Line="Get a knige", Platform="Knife and chopping board"},
+                new Command{Id=2, HowTo="Make a cup of tea", Line="Place teabag in cup", Platform="Kettle and cup"}
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kettle and pan"};
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Profiles/CommandsProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using Commander.Dto;
+using Commander.Models;
+
+namespace Commander.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            CreateMap<Command, CommandReadDto>();
+            CreateMap<CommandCreateDto, Command>();
+        }
+    }
+}
